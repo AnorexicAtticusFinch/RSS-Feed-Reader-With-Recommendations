eb9f1c8cd05c831c31c14a25bd8be386127102fc
Sriram2001 <47755579+Sriram2001@users.noreply.github.com>
2019-03-26 13:24:47 +0530

    Update views.py

--- sources/views.py
+++ sources/views.py
@@ -12,6 +12,10 @@ def home_view(request,*args,**kwargs):
     name=form.cleaned_data['file_name']
     l=form.cleaned_data['link']
     f= Feed_Source(name,l)
-    context ={'form':form,'link1':f.articles[1].link,'description1':f.articles[1].description,'title1':f.articles[1].title}
+    context ={'form':form,}
+    for i in range(1, len(f.articles) + 1):
+        context['link'+str(i)]=f.articles[i-1].link
+        context['title'+str(i)]=f.articles[i-1].title
+        context['description'+str(i)]=f.articles[i-1].description
     #print(f.articles)
     return render(request,"home.html",context)
