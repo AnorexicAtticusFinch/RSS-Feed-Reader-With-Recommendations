a4e6d3b42144be7fc03df65f224056728128538e
Sriram2001 <47755579+Sriram2001@users.noreply.github.com>
2019-03-26 21:46:45 +0530

    Update models.py

--- sources/models.py
+++ sources/models.py
@@ -4,4 +4,4 @@ from django.db import models
 
 class Source(models.Model):
     link      = models.CharField(max_length = 150,default='https://timesofindia.indiatimes.com/rssfeedstopstories.cms')
-    file_name = models.CharField(max_length=50,default='TOP_STORIES')
+    name = models.CharField(max_length=50,default='TOP_STORIES')
