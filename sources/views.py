4adc5879d7ea2bef4e8b3866398ba8a4fc9e298e
Sriram2001 <47755579+Sriram2001@users.noreply.github.com>
2019-03-26 21:45:33 +0530

    Update views.py

--- sources/views.py
+++ sources/views.py
@@ -7,15 +7,20 @@ from class_definitions import Feed_Source
 
 def home_view(request,*args,**kwargs):
     form = SourceForm(request.POST or None)
+    name=''
+    l=''
     if form.is_valid():
         form.save()
-    name=form.cleaned_data['file_name']
-    l=form.cleaned_data['link']
-    f= Feed_Source(name,l)
     context ={'form':form,}
-    for i in range(1, len(f.articles) + 1):
-        context['link'+str(i)]=f.articles[i-1].link
-        context['title'+str(i)]=f.articles[i-1].title
-        context['description'+str(i)]=f.articles[i-1].description
-    #print(f.articles)
+    try:   
+        name=form.cleaned_data['name']
+        l=form.cleaned_data['link']
+        f= Feed_Source(name,l)
+        for i in range(1, len(f.articles) + 1):
+            context['link'+str(i)]=f.articles[i-1].link
+            context['title'+str(i)]=f.articles[i-1].title
+            context['description'+str(i)]=f.articles[i-1].description
+    except Exception:   
+        print("")
+    #print(f.articles)'''
     return render(request,"home.html",context)
