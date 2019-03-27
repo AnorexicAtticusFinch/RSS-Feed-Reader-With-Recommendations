8329b6ee80d6ca4e94faf91ceecad52e2212ed9f
AnorexicAtticusFinch <47782857+AnorexicAtticusFinch@users.noreply.github.com>
2019-03-27 15:02:26 +0530

    Update recommendations.py

--- recommendations.py
+++ recommendations.py
@@ -16,34 +16,49 @@ class Predefined_Sources:
     def __init__(self):
 
         self.list_of_sources = []
-        file = open("list_of_sources", "r")
+        file = open("list_of_sources.txt", "r")
+        count = 1
         for source in file:
+
+            if source[-1] == "\n":
+
+                source = source[ : -1]
             
-            self.list_of_sources.append(Feed_Source(source, source))
+            self.list_of_sources.append(Feed_Source(str(count), source))
+            count += 1
+
         file.close()
 
     def find_best_fits(self, given_source : Feed_Source) -> "list of Feed_Sources":
 
-        least_distance = None
-        sources = []
+        if given_source in self.list_of_sources:
+
+            self.list_of_sources.remove(given_source)
+
+        min_distance_0 = None
+        min_distance_1 = None
+
+        sources = [None, None]
 
         for source in self.list_of_sources:
 
             current_distance = dist_between_vectors(given_source.feature_vector, source.feature_vector)
 
-            if least_distance is None:
+            if min_distance_0 is None and min_distance_1 is None:
 
-                least_distance = current_distance
-                sources.append(source)
-
-            elif current_distance < least_distance:
-                
-                least_distance = current_distance
-                sources = []
-                sources.append(source)
+                min_distance_0 = current_distance
+                sources[0] = source
             
-            elif current_distance == least_distance:
+            elif (min_distance_1 is None or current_distance < min_distance_1) and not current_distance < min_distance_0 :
 
-                sources.append(source)
-        
+                min_distance_1 = current_distance
+                sources[1] = source
+
+            elif current_distance < min_distance_0:
+
+                min_distance_1 = min_distance_0
+                min_distance_0 = current_distance
+                sources[1] = sources[0]
+                sources[0] = source
+   
         return sources
