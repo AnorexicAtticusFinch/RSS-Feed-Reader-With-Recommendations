bfc426309dd675f1307433f08a01623083620fe2
Sriram2001 <sriramudupi19@gmail.com>
2019-03-30 00:25:52 +0530

    ...

--- sources/views.py
+++ sources/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Source
 from .forms import SourceForm
+from recommendations import Predefined_Sources
 from class_definitions import Feed_Source
 
 # Create your views here.
@@ -28,10 +29,28 @@ def home_view(request,*args,**kwargs):
 
         f= Feed_Source(name,l)
 
+        sl= Predefined_Sources()
+
+        lst = sl.find_best_fits(f)
+
         for i in range(1, len(f.articles) + 1):
             context['link'+str(i)]=f.articles[i-1].link
             context['title'+str(i)]=f.articles[i-1].title
             context['description'+str(i)]=f.articles[i-1].description
+
+
+        for i in range(1,len(lst[0].articles)+1):
+            context[str(i)+'link']=lst[0].articles[i-1].link
+            context[str(i)+'title']=lst[0].articles[i-1].title
+            context[str(i)+'description']=lst[0].articles[i-1].description
+
+        for i in range(1,len(lst[1].articles)+1):
+            context[str(i)+'_link']=lst[1].articles[i-1].link
+            context[str(i)+'_title']=lst[1].articles[i-1].title
+            context[str(i)+'_description']=lst[1].articles[i-1].description
+
+
+        
 
     except Exception:   
         print("")
