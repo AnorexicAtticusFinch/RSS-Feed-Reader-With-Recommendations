e958d432999f1200fdbf291e506d3ea01166704e
Sriram2001 <47755579+Sriram2001@users.noreply.github.com>
2019-03-29 22:54:17 +0530

    Update views.py

--- sources/views.py
+++ sources/views.py
@@ -10,17 +10,31 @@ def home_view(request,*args,**kwargs):
     name=''
     l=''
     if form.is_valid():
-        form.save()
+        #form.save()
+        {}
     context ={'form':form,}
     try:   
         name=form.cleaned_data['name']
         l=form.cleaned_data['link']
+        k=False
+    
+        #form.save()
+        for i in Source.objects.all():
+            if( l == i.link):
+                k=True
+        
+        if( k != True):
+            form.save()
+
         f= Feed_Source(name,l)
+
         for i in range(1, len(f.articles) + 1):
             context['link'+str(i)]=f.articles[i-1].link
             context['title'+str(i)]=f.articles[i-1].title
             context['description'+str(i)]=f.articles[i-1].description
+
     except Exception:   
         print("")
     #print(f.articles)'''
+    
     return render(request,"home.html",context)
