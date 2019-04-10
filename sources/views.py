bd6181b30221507a91e9f630fbc303cbd47f9f94
Sriram2001 <47755579+Sriram2001@users.noreply.github.com>
2019-04-10 20:12:34 +0530

    Update views.py

--- sources/views.py
+++ sources/views.py
@@ -6,17 +6,12 @@ from class_definitions import Feed_Source
 from django.template.context import RequestContext
 
 
-# Create your views here.
-
 def home_view(request,*args,**kwargs):
     form = SourceForm(request.POST or None)
     name=''
-    l=''
-    if form.is_valid():
-        #form.save()
-        {}
+    l='' 
     context ={'form':form,}
-    try:   
+    if form.is_valid():   
         name=form.cleaned_data['name']
         l=form.cleaned_data['link']
         k=False
@@ -56,11 +51,38 @@ def home_view(request,*args,**kwargs):
         context['Source']=sourc
         
 
-    except Exception:   
-        print("")
+    else:
+        name="Top Stories"
+        l="https://timesofindia.indiatimes.com/rssfeedstopstories.cms"
+   
+        f= Feed_Source(name,l)
+
+        sl= Predefined_Sources()
+
+        lst = sl.find_best_fits(f)
+
+        for i in range(1, len(f.articles) + 1):
+            context['link'+str(i)]=f.articles[i-1].link
+            context['title'+str(i)]=f.articles[i-1].title
+            context['description'+str(i)]=f.articles[i-1].description
+
+
+        for i in range(1,len(lst[0].articles)+1):
+            context[str(i)+'link']=lst[0].articles[i-1].link
+            context[str(i)+'title']=lst[0].articles[i-1].title
+            context[str(i)+'description']=lst[0].articles[i-1].description
+
+        for i in range(1,len(lst[1].articles)+1):
+            context[str(i)+'_link']=lst[1].articles[i-1].link
+            context[str(i)+'_title']=lst[1].articles[i-1].title
+            context[str(i)+'_description']=lst[1].articles[i-1].description
+
+        sourc=Source.objects.all()
+        context['Source']=sourc
+        
     #print(f.articles)'''
     
-    return render(request,"home.html",context)
+    return render(request,"base1.html",context)
 
 
 def item_view(request,pk):
@@ -94,4 +116,5 @@ def item_view(request,pk):
         context[str(i)+'_description']=lst[1].articles[i-1].description
         
     
-    return render(request,"saved.html",context) 
+    return render(request,"source1.html",context)
+    
