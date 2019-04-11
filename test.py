583666943919e835a539448e6b67bf1b776e0805
AnorexicAtticusFinch <47782857+AnorexicAtticusFinch@users.noreply.github.com>
2019-04-11 11:14:04 +0530

    Add files via upload

--- test.py
+++ test.py
@@ -2,10 +2,12 @@ from class_definitions import *
 from recommendations import *
 
 sl = Predefined_Sources()
-#gs = Feed_Source("test", "http://feeds.feedburner.com/ndtvsports-cricket")
-gs = Feed_Source("test", "http://feeds.feedburner.com/ndtvnews-top-stories")
-#gs = Feed_Source("test", "https://timesofindia.indiatimes.com/rssfeeds/5880659.cms")
-#gs = Feed_Source("test", "https://timesofindia.indiatimes.com/rssfeeds/1081479906.cms")
+
+#gs = Feed_Source("test", "http://feeds.feedburner.com/ndtvsports-cricket") #CRICKET NDTV
+#gs = Feed_Source("test", "http://feeds.feedburner.com/ndtvnews-top-stories") #TOP STORIES NDTV
+gs = Feed_Source("test", "https://timesofindia.indiatimes.com/rssfeeds/5880659.cms") #SCI&TECH TOI
+#gs = Feed_Source("test", "https://timesofindia.indiatimes.com/rssfeeds/1081479906.cms") #ENT TOI
+#gs = Feed_Source("test", "https://timesofindia.indiatimes.com/rssfeeds/1898055.cms") #BUS TOI
 
 for source in sl.list_of_sources:
     print(source.feature_vector)
@@ -29,8 +31,9 @@ print()
 print()
 
 for source in lst:
-    for article in source.articles:
-        print(article.title)
-    print()
-    print()
-    print()
+        print(source.link)
+        for article in source.articles:
+                print(article.title)
+        print()
+        print()
+        print()
